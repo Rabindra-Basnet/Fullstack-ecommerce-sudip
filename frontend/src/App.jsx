33ffc03398f97604a3ad43608e3c9deaa5896b78
D:/Git Folder/Git/ <sudippathak727@gmail.com>
2024-07-18 17:23:56 +0545

    edited main, HomePage and products.jsx to add router and did that by imprting router package and then outlet from that pakage.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,14 @@
 import { Container, Row, Col } from "react-bootstrap";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Product from "./components/Product";
-import products from "./products";
-import ProductPage from "./pages/ProductPage";
-
+import { Outlet } from "react-router-dom";
 function App() {
   return (
     <>
       <h1> Ecommerce</h1>
       <Header/>
       <Container className="my-3">
-      {/* <ProductPage/> */}
-        <Row>
-          {
-            products.map(product => (
-              <Col sm={12} md={6} lg={4} xlg={3}>
-                <Product product={product}/>
-              </Col>
-            ))
-          }
-        </Row>
-        {/* <Product product={products[3]} /> */}
+        <Outlet/>
       </Container>
       <Footer/>
     </>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {RouterProvider, createBrowserRouter} from 'react-router-dom'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
+import HomePage from './pages/HomePage.jsx'
+import ProductPage from './pages/ProductPage.jsx'
 
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<App/>,
+    children:[
+    {
+      path:"",
+      element:<HomePage/>
+    },
+    {
+      path:"product",
+      element:<ProductPage/>,
+    },
+  ]
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router}>
+  </RouterProvider> 
+);
